ebc352dde7d029b1be71aef73997c46780514ce8
Aimee Knight <aimeeknight83@gmail.com>
2013-04-26 17:11:42 -0400

    Added custom routes for auth and feed.

--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -3,10 +3,10 @@ require 'test_helper'
 class StatusTest < ActiveSupport::TestCase
     test "A status should not be blank" do
         status = Status.new
-        #status.content = "H"
+        status.content = "H"
         assert !status.save #call status.save and flip return value
-        #will fail (saves the status with one letter
-        #prior to adding the vailidation)
-        #assert !status.errors[:content].empty?
+        # will fail (saves the status with one letter
+        # prior to adding the vailidation)
+        assert !status.errors[:content].empty?
     end
 end
