637e2946ddf1aa40d3df4439299caa8d116a3b00
Aimee Knight <AimeeKnight83@gmail.com>
2013-07-11 10:58:18 -0400

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,6 +51,8 @@ Treebook::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   config.action_mailer.raise_delivery_errors = true
 
+  config.action_mailer.perform_deliveries = true
+  
   # Enable threaded mode
   # config.threadsafe!
 
