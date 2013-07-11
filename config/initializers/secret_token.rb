8c84139ceeec70fb5efdb8286986e8b4c022f2ac
Aimee Knight <AimeeKnight83@gmail.com>
2013-07-10 20:07:30 -0400

    add env vars

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Treebook::Application.config.secret_token = '39ce778047ce9633d2b6860ca02ab124fe23bb332e42e03e97cbb2b94a9fb297382f9708421859ffe1d9740fd4a9e5db200dee5d8382a47ed257cc6a365bf495'
+Treebook::Application.config.secret_token = ENV["SECRET_TOKEN"]
