e9a191b4a9d3c6418fd06588b7bec1243bbebcb8
Aimee Knight <AimeeKnight83@gmail.com>
2013-07-10 21:26:03 -0400

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,14 +65,14 @@ Treebook::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'http://ancient-mountain-1843.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'ancient-mountain-1843.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp 
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "http://ancient-mountain-1843.herokuapp.com",
+  domain: "ancient-mountain-1843.herokuapp.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
