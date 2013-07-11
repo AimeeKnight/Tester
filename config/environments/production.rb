f83aef99036a796bffef20b62e969346ef60557f
Aimee Knight <AimeeKnight83@gmail.com>
2013-07-10 21:33:34 -0400

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -49,7 +49,7 @@ Treebook::Application.configure do
   # config.assets.precompile += %w( search.js )
 
   # Disable delivery errors, bad email addresses will be ignored
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable threaded mode
   # config.threadsafe!
@@ -72,7 +72,7 @@ Treebook::Application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "ancient-mountain-1843.herokuapp.com",
+  domain: "gmail.com",
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
