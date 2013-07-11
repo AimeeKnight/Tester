37c24ee7a5911108457a09943f8bda7a29dbe253
Aimee Knight <AimeeKnight83@gmail.com>
2013-07-11 09:47:05 -0400

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,12 +67,12 @@ Treebook::Application.configure do
 
   config.action_mailer.default_url_options = { :host => 'http://ancient-mountain-1843.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: "gmail.com",
+  domain: "gmail.com", 
   authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
