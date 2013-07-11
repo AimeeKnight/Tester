01f0fec4cbcd787948dcd207871ea53da6e6c650
Aimee Knight <AimeeKnight83@gmail.com>
2013-07-11 13:09:29 -0400

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Treebook::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 
-  config.action_mailer.default_url_options = { :host => 'smtp.sendgrid.net'} 
+  config.action_mailer.default_url_options = { :host => 'safe-ridge-9329.herokuapp.com'} 
 
   config.action_mailer.delivery_method = :smtp
 
